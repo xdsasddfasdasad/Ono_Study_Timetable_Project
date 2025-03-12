f383d83154f6b3c8e22d9655eae97dcc92dc42f6
xdsasddfasdasad <itaywass@gmail.com>
2025-03-12 09:35:07 +0200

    fix mui

--- Ono_Study_Timetable_Project/src/components/Header.jsx
+++ Ono_Study_Timetable_Project/src/components/Header.jsx
@@ -1,14 +1,14 @@
-import { AppBar, Typography } from '@mui/material'
+import { AppBar, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Header() {
   return (
-   <AppBar>
-        <Toolbar>
-            <Typography variant='h6' component="div">
-                Header
-            </Typography>
-        </Toolbar>
-   </AppBar> 
+    <AppBar>
+      <Toolbar>
+        <Typography variant='h6' component="div">
+          Header
+        </Typography>
+      </Toolbar>
+    </AppBar> 
   );
 }
