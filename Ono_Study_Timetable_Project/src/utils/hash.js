32773978f7b89f3ba10d2b06d5a85a6dd54244a1
xdsasddfasdasad <itaywass@gmail.com>
2025-04-05 14:52:44 +0300

    update hash utils

--- Ono_Study_Timetable_Project/src/utils/hash.js
+++ Ono_Study_Timetable_Project/src/utils/hash.js
@@ -1,9 +1,8 @@
-const hashPassword = async (password) => {
+export async function hashPassword(password) {
     const encoder = new TextEncoder();
     const data = encoder.encode(password);
     const hashBuffer = await crypto.subtle.digest("SHA-256", data);
-    return Array.from(new Uint8Array(hashBuffer))
-      .map((b) => b.toString(16).padStart(2, "0"))
-      .join("");
-  };
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
+  }
   
