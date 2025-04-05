aacfef5e27f5fb517305f4a1eaef6cb5ead62156
xdsasddfasdasad <itaywass@gmail.com>
2025-04-05 13:13:58 +0300

    remove css

--- Ono_Study_Timetable_Project/src/App.jsx
+++ Ono_Study_Timetable_Project/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
-import "./HeaderNavigationBar.css"; // או tailwind במידת הצורך
 
 const HeaderNavigationBar = () => {
   const navigate = useNavigate();
