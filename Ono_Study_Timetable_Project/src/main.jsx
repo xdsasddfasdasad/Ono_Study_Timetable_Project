ae07f466d89b4216dcbedba0b86fa6e8aec7a677
xdsasddfasdasad <itaywass@gmail.com>
2025-04-05 11:24:15 +0300

    fix browserrouter

--- Ono_Study_Timetable_Project/src/main.jsx
+++ Ono_Study_Timetable_Project/src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {BrwoserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/globals.css';  // קובץ עיצוב עיקרי
 import './index.css';           // reset בסיסי אם צריך
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrwoserRouter>
+    <BrowserRouter>
       <App />
-    </BrwoserRouter>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
+
