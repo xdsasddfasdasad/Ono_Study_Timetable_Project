6cc23d40d53d67fa518e8f898ac0e928a34a49b6
xdsasddfasdasad <itaywass@gmail.com>
2025-04-05 11:33:42 +0300

    update pages and components

---
+++ Ono_Study_Timetable_Project/src/components/modals/StudentManagementModal.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StudentManagementModal() {
+  return (
+    <div>StudentManagementModal</div>
+  )
+}
--- Ono_Study_Timetable_Project/src/pages/SystemInfoDashboardPage.jsx
+++ Ono_Study_Timetable_Project/src/pages/SystemInfoDashboardPage.jsx
@@ -1,14 +1,7 @@
-// src/pages/SystemInfoDashboardPage.jsx
+import React from 'react'
 
-import React from "react";
-
-const SystemInfoDashboardPage = () => {
+export default function SystemInfoDashBoardPage() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>System Info Dashboard</h1>
-      <p>This page will show insights, stats and summaries about your schedule.</p>
-    </div>
-  );
-};
-
-export default SystemInfoDashboardPage;
+    <div>SystemInfoDashBoardPage</div>
+  )
+}
